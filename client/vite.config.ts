2fe435eac9ac1ba668632fbc2679ad42bb57694d
Alex Bennett <alexandergraybennett@gmail.com>
2022-09-04 10:24:28 -0600

    task: adds rust wasm to js toolchain

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-
+import wasmPack from 'vite-plugin-wasm-pack';
 
 export default defineConfig(() => {
   return {
-    
-    plugins: [
-      qwikVite(),
-      
-    ],
+    plugins: [qwikVite(), wasmPack('./src/grid')],
   };
 });
